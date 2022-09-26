0524010531f211c7b34534705179a6d386377ac0
Pushpam46 <chodankarpushpam46@gmail.com>
2022-09-26 13:20:18 +0530

    First changes 4

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,7 +10,7 @@ function Header() {
     <header>
         <div className='container header__container'>
             <h5>Hello I'm</h5>
-            <h1>hrutu Swar</h1>
+            <h1>Pushpam Chodankar</h1>
             <h5 className='text-light'>Software Developer</h5>
             <CTA />
 
